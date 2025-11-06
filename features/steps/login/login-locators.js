5ce19bbbdf00d54d5f6d3dbb945d2ce1fc1c18a1
titor oopart <johnpiterzon@gmail.com>
2025-11-05 23:51:11 -0500

    TestCafe login base scenario implemented.

---
+++ features/steps/login/login-locators.js
@@ -0,0 +1,9 @@
+export const LoginLocators = {
+  userName: '[data-testid="username"]',
+  password: '[data-testid="password"]',
+  continueButton: "#login-submit",
+  avatarMenu: "#header-member-menu-avatar",
+  emailTextField:
+    '[data-testid="account-menu-account-section"] div[class="RpQsLvDvZmhbBg"]',
+  trelloLogo: '[data-testid="team25-header-logo"]',
+};
