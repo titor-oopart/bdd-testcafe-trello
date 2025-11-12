fecf58f96f5e9ad2e3f2ebace85900f636f51987
titor oopart <johnpiterzon@gmail.com>
2025-11-11 22:58:18 -0500

    Login Logout hooks implemented.

--- features/steps/login/login-steps.js
+++ features/steps/login/login-steps.js
@@ -7,6 +7,14 @@ When(/I enter valid credentials/, async (t) => {
   await loginPage.login();
 });
 
+When(/I logout/, async (t) => {
+  await loginPage.logout();
+});
+
 Then(/I should see that the user is logged in successfully/, async (t) => {
   await loginPage.verifyUserLogged();
+});
+
+Then(/I am redirected to trello landing page/, async (t) => {
+  await loginPage.verifyLogout();
 });
